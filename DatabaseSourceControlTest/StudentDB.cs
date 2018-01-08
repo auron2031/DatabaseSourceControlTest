de14e4b528d93643ac865afb46300c4eea862e08
TDCheese <haha@haha.ha>
2018-01-08 14:31:02 -0800

    Added StudentDB class with all CRUD functionality.

--- DatabaseSourceControlTest/StudentDB.cs
+++ DatabaseSourceControlTest/StudentDB.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace DatabaseSourceControlTest
 {
-    class StudentDB
+    /// <summary>
+    /// Contains CRUD functionality for
+    /// students
+    /// </summary>
+    static class StudentDB
     {
+        /// <summary>
+        /// Gets all students from the database
+        /// </summary>
+        /// <returns></returns>
+        public static List<string> GetStudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Adds a student to the database
+        /// </summary>
+        public static void AddStudent()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
